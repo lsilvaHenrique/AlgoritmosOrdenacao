e65618a5a257a61158d45b6d600ff916c1d09be0
lucas.henrique <lucas.henrique@mobility_0105.graded.school>
2023-03-14 11:50:50 -0300

    Algoritmos de Ordenacao

--- OrdenaçõesED/src/sp/fateczl/lucas/Ordenacao/OrdenacaoController.java
+++ OrdenaçõesED/src/sp/fateczl/lucas/Ordenacao/OrdenacaoController.java
@@ -5,65 +5,75 @@ public class OrdenacaoController {
 	public OrdenacaoController() {	}
 	
 	// Implementacao do BubbleSort
-	public void bubbleSort(int vetor[]) {
-		int aux;
-		
-		for (int i = 0; i < vetor.length; i++) {
-			if (i == (vetor.length - 1)) continue;
-			
-			if (vetor[i] > vetor[i+1]) {
-				aux = vetor[i];
-				vetor[i] = vetor[i+1];
-				vetor[i+1] = aux;
-			}
-		}
+	public void bubbleSort(int v[]) {
+		 for(int i = 0; i < v.length - 1; i++) {    
+		      for(int j = 0; j < v.length - 1 - i; j++) {	       
+		        if(v[j] > v[j + 1]) {
+		          int aux = v[j];
+		          v[j] = v[j + 1];
+		          v[j + 1] = aux;
+		        }	
+		      }
+		 }
 	}
 	// Fim implementacao
 	
 	// Implementacao do MergeSort
-	public int[] mergeSort(int vetor[], int inicio, int fim) {
-		if (inicio > fim) {
-			int aux = inicio;
-			inicio = fim;
-			fim = aux;
-		}
-		
-		int meio = (inicio + fim) / 2;
-		mergeSort(vetor, inicio, meio);
-		mergeSort(vetor, meio+1, fim);
-		intercalarVetores(vetor, inicio, meio, fim);
-		
-		return vetor;
+	
+	public void mergeSort(int tamanho, int[] vetor) {
+		int elementos = 1;
+	    int inicio, meio, fim;
+	    
+	    while(elementos < tamanho) {
+	    	inicio = 0;
+	    
+	    while(inicio + elementos < tamanho) { 
+	    	meio = inicio + elementos;
+	    	fim = inicio + 2 * elementos;
+	        
+	    	if(fim > tamanho)
+	    		fim = tamanho; 
+	        	intercala(vetor, inicio, meio, fim);
+	        	inicio = fim;
+	      	}
+	      	elementos = elementos * 2;
+	   }
 	}
 
-	private void intercalarVetores(int[] vetor, int inicio, int meio, int fim) {
-		int novoVetor[] = new int[vetor.length];
-		
-		for (int i = inicio; i <= fim; i++) {
-			novoVetor[i] = vetor[i];
-		}
-		
-		int esquerda = inicio;
-		int direita = meio + 1;
-		
-		for (int i = inicio; i <= fim; i++) {
-			if (esquerda > meio) {
-				vetor[i] = novoVetor[direita];
-				direita++;
-			} else if (direita > fim) {
-				vetor[i] = novoVetor[esquerda];
-				esquerda++;
-			} else if (novoVetor[esquerda] < novoVetor[direita]) {
-				vetor[i] = novoVetor[esquerda];
-				esquerda++;
+	private static void intercala(int[] vetor, int inicio, int meio, int fim) { 
+		int novoVetor[] = new int[fim - inicio];  
+		int i = inicio;
+    	int m = meio;
+		int pos = 0;
+			
+		while(i < meio && m < fim) {
+			if(vetor[i] <= vetor[m]) {
+				novoVetor[pos] = vetor[i];
+				pos = pos + 1;
+				i = i + 1;
+     
 			} else {
-				vetor[i] = novoVetor[direita];
-				direita++;
+				novoVetor[pos] = vetor[m];
+				pos = pos + 1;
+				m = m + 1;
 			}
 		}
-		
+    
+		while(i < meio) {
+			novoVetor[pos] = vetor[i];
+			pos = pos + 1;
+			i = i + 1;
+		}
+    
+		while(m < fim) {
+			novoVetor[pos] = vetor[m];
+			pos = pos + 1;
+			m = m + 1;
+		}
+    
+		for(pos = 0, i = inicio; i < fim; i++, pos++) {
+			vetor[i] = novoVetor[pos];
+		}
 	}
-	
 }
-	
 	// Fim implementacao
